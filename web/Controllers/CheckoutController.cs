ade1cddc0aadff5cc6d091d8d200c1c14147d57f
Michael Arrabito <mikearrabito@gmail.com>
2020-11-07 23:33:57 -0500

    added checkout and confirmation page(final checkout)
    need to finish final confirmation page

---
+++ web/Controllers/CheckoutController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ShoppingLibrary.Logic;
+using ShoppingLibrary.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class CheckoutController : Controller
+    {
+        // GET: Checkout
+        public ActionResult Index()
+        {
+            var customerId = HttpContext.Session.GetInt32("customer");
+            if (customerId != null)
+            {
+                CartManager cartManager = CustomerManager.GetCustomerCart((int)customerId);
+                if (cartManager != null)
+                    ViewData["ShoppingCart"] = cartManager.shoppingCart.Items.ToList();
+            }
+
+            return View();
+        }
+
+        // GET: Checkout/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        public ActionResult Confirm()
+        {
+            return View();
+        }
+
+        // GET: Checkout/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Checkout/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Checkout/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Checkout/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Checkout/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Checkout/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
