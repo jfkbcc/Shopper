7d0da77bd4a3b0590b8bfd103b476856ca066637
joey <1166538+xtjoeytx@users.noreply.github.com>
2020-12-03 19:54:18 -0500

    Fixed session timeout issue, kept clearing shopping cart after 10 seconds. Now it will clear after 20 minutes

--- web/Startup.cs
+++ web/Startup.cs
@@ -30,7 +30,7 @@ namespace WebApplication1
 
             services.AddSession(options =>
             {
-                options.IdleTimeout = TimeSpan.FromSeconds(10);
+                options.IdleTimeout = TimeSpan.FromMinutes(20);
                 options.Cookie.HttpOnly = true;
                 options.Cookie.IsEssential = true;
             });
